1026ae74fccccb6d32740400198c6a0ce6af75eb
Felix Tripier <ftripier@gmail.com>
2019-01-24 16:02:23 -0800

    Better UI for long gate names

--- src/components/gate/index.stories.tsx
+++ src/components/gate/index.stories.tsx
@@ -3,6 +3,8 @@ import { storiesOf } from '@storybook/react';
 
 import Gate from './index';
 
-storiesOf('Gate', module).add('Hadamard Gate', () => (
-  <Gate name="H" position={[0, 0]} size={[50, 50]} />
-));
+storiesOf('Gate', module)
+  .add('Hadamard Gate', () => <Gate name="H" position={[0, 0]} size={[50, 50]} />)
+  .add('With truncated name', () => (
+    <Gate name="HADAMARD_DIFFUSION_GATE" position={[0, 0]} size={[50, 200]} />
+  ));
