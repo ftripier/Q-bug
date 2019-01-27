813520b041ef56036778a9ae94f6b6e9d751af04
Felix Tripier <ftripier@gmail.com>
2019-01-26 18:07:39 -0800

    Wire index indicators

--- src/components/circuit/index.tsx
+++ src/components/circuit/index.tsx
@@ -3,14 +3,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Gate from '../gate';
 import WireSegment from '../wire-segment';
-import { GateLayout, WireSegmentLayout } from '../../state/ui/types';
+import { GateLayout, WireSegmentLayout, WireLayout } from '../../state/ui/types';
 import { getCircuitLayout, getWindowSize } from '../../state/ui/selectors/layout';
 import { AppState } from '../../state/types';
 
 export const StatelessCircuit = React.memo(
-  ({ gates, wireSegments }: { gates: GateLayout[]; wireSegments: WireSegmentLayout[] }) => (
+  ({
+    gates,
+    wireSegments,
+    wires
+  }: {
+    gates: GateLayout[];
+    wireSegments: WireSegmentLayout[];
+    wires: WireLayout[];
+  }) => (
     <div className="circuit-canvas">
       <div className="circuit-wire-layer">
+        {wires.map(({ top, left }, i) => (
+          <div
+            className="wire-index-indicator"
+            style={{
+              top: top,
+              left: left
+            }}
+          >
+            {i}
+          </div>
+        ))}
         {wireSegments.map(({ width, top, left, probabilityZero, id }) => (
           <WireSegment
             key={id}
@@ -38,12 +57,13 @@ export const StatelessCircuit = React.memo(
 );
 
 const mapStateToProps = (state: AppState) => {
-  const { wireSegments, gates } = getCircuitLayout(state);
+  const { wireSegments, gates, wires } = getCircuitLayout(state);
   const windowSize = getWindowSize(state);
   return {
     wireSegments,
     gates,
-    windowSize
+    windowSize,
+    wires
   };
 };
 
