5a649f09fd504d445aaf1a10b8a6600997604fa6
Felix Tripier <ftripier@gmail.com>
2019-02-03 05:09:31 -0800

    Minor updates for FOSDEM demo

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 A work-in-progress web-based visual debugger for Quil programs.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Development workflow for UI
 
-1. Run the server with ./scripts/run.sh
-2. Once shelled into the server, run `pipenv run start`
-3. In a different terminal, on your host machine, start the dev server with `REACT_APP_DEBUG_SERVER_PORT=8765 npm run start`
+1. If you haven't already, build the docker image with `docker build . -t qbugserver`. This
+   builds a Q-bug image that serves the UI on port 8001 and the websocket
+   server on port 8765.
+2. Run the server with ./scripts/run.sh
+3. Once shelled into the server, run `pipenv run start`
+4. In a different terminal, on your host machine, start the dev server with `REACT_APP_DEBUG_SERVER_PORT=8765 npm run start`
 
 ## Available Scripts
 
--- src/components/wire-segment/index.stories.tsx
+++ src/components/wire-segment/index.stories.tsx
@@ -5,6 +5,58 @@ import { action } from '@storybook/addon-actions';
 import { StatelessWireSegment as WireSegment } from './index';
 import LiveWire from '../wire-segment/LiveWire';
 
+class WireSlider extends React.PureComponent {
+  state: {
+    probabilityZero: number;
+  };
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      probabilityZero: 1
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ probabilityZero: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          <span
+            style={{
+              display: 'inline-block',
+              width: '36px'
+            }}
+          >
+            {this.state.probabilityZero}
+          </span>
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            value={this.state.probabilityZero}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <WireSegment
+          openTooltip={action('open tooltip')}
+          closeTooltip={action('close tooltip')}
+          position={[0, 50]}
+          width={150}
+          probabilityZero={this.state.probabilityZero}
+          id="rofl"
+        />
+      </div>
+    );
+  }
+}
+
 storiesOf('WireSegment', module)
   .add('Regular Wire Segment', () => (
     <WireSegment
@@ -37,4 +89,5 @@ storiesOf('WireSegment', module)
     />
   ))
   .add('LiveWire - sparse', () => <LiveWire density={0} width={100} />)
-  .add('LiveWire - dense', () => <LiveWire density={1} width={100} />);
+  .add('LiveWire - dense', () => <LiveWire density={1} width={100} />)
+  .add('WireSegment w/ probability slider', () => <WireSlider />);
